5776111eab002e5d26e3fce7f325aa1a8fd88192
Astrid Chen <ry60577@gapp.nthu.edu.tw>
2022-06-24 13:44:53 +0800

    feat:主題館頁面

--- src/pages/ThemePage.vue
+++ src/pages/ThemePage.vue
@@ -3,101 +3,99 @@
     class="q-pa-md theme_wrapper"
     :class="{ 'pw-sm': isMobile, 'pw-xl': !isMobile }"
   >
-    <q-carousel
-      swipeable
-      animated
-      v-model="slide"
-      :autoplay="autoplay"
-      ref="carousel"
-      infinite
-    >
-      <q-carousel-slide
-        v-for="theme of themeData"
-        :key="theme.name"
-        :img-src="require(theme.imgUrl)"
+    <div class="theme_carousel theme_bg">
+      <q-carousel
+        swipeable
+        animated
+        v-model="slide"
+        ref="carousel"
+        :arrows="isMobile"
       >
-        <!-- <ThemeCard :theme="theme" /> -->
-      </q-carousel-slide>
-      <!-- <q-carousel-slide
-        :name="1"
-        img-src="https://cdn.quasar.dev/img/mountains.jpg"
-      />
-      <q-carousel-slide
-        :name="2"
-        img-src="https://cdn.quasar.dev/img/parallax1.jpg"
-      />
-      <q-carousel-slide
-        :name="3"
-        img-src="https://cdn.quasar.dev/img/parallax2.jpg"
-      />
-      <q-carousel-slide
-        :name="4"
-        img-src="https://cdn.quasar.dev/img/quasar.jpg"
-      /> -->
+        <template v-for="theme in themeData" :key="theme">
+          <q-carousel-slide :name="theme.name">
+            <div class="carousel_card">
+              <div class="desc_wrap">
+                <h3>{{ theme.chineses_name }}</h3>
+                <div class="desc">{{ theme.desc }}</div>
+                <q-btn
+                  icon-right="search"
+                  :label="`搜尋${theme.chineses_name}`"
+                />
+              </div>
+              <img :src="theme.imgUrl" :alt="`${theme.name}|PetNi 陪你`" />
+            </div>
+          </q-carousel-slide>
+        </template>
 
-      <template v-slot:control>
-        <q-carousel-control
-          position="top-right"
-          :offset="[18, 18]"
-          class="text-white rounded-borders"
-          style="background: rgba(0, 0, 0, 0.3); padding: 4px 8px"
-        >
-          <q-toggle
-            dense
-            dark
-            color="orange"
-            v-model="autoplay"
-            label="Auto Play"
-          />
-        </q-carousel-control>
-
-        <q-carousel-control
-          position="bottom-right"
-          :offset="[18, 18]"
-          class="q-gutter-xs"
-        >
-          <q-btn
-            push
-            round
-            dense
-            color="orange"
-            text-color="black"
-            icon="arrow_left"
-            @click="$refs.carousel.previous()"
-          />
-          <q-btn
-            push
-            round
-            dense
-            color="orange"
-            text-color="black"
-            icon="arrow_right"
-            @click="$refs.carousel.next()"
-          />
-        </q-carousel-control>
-      </template>
-    </q-carousel>
+        <template v-slot:control v-if="!isMobile">
+          <q-carousel-control
+            position="bottom-right"
+            :offset="[18, 18]"
+            class="q-gutter-xs"
+          >
+            <q-btn
+              push
+              square
+              dense
+              color="white"
+              text-color="black"
+              icon="arrow_left"
+              @click="$refs.carousel.previous()"
+            />
+            <q-btn
+              push
+              square
+              dense
+              color="white"
+              text-color="black"
+              icon="arrow_right"
+              @click="$refs.carousel.next()"
+            />
+          </q-carousel-control>
+        </template>
+      </q-carousel>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { useStore } from "vuex";
 import themeList from "../json_data/theme_data.json";
-import ThemeCard from "../components/ThemeCard.vue";
+import { useStore } from "vuex";
 
 const store = useStore();
 const isMobile = store.getters["common/getIsMobile"];
 const themeData = themeList.map((item) => {
-  item.imgUrl = `../assets/image/${item.name}.svg`;
+  item.imgUrl = require(`../assets/image/${item.name}.svg`);
+  return item;
 });
-
-const slide = ref(themeData.at(0).name);
+const slide = ref("orange_tabby_cat");
 const autoplay = ref(false);
 </script>
 
 <style lang="scss" scoped>
 .theme_wrapper {
+  height: 100vh;
   background-color: #e5e5e5;
+
+  .carousel_card {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    // grid-template-rows: 70% 30%;
+    // div:first-child {
+    //   order: 2;
+    // }
+  }
+  .theme_bg {
+    background-image: url("../assets/image/cat_chineses.svg");
+    background-repeat: no-repeat;
+    background-position: 60% 20%;
+  }
+  :deep {
+    .q-carousel {
+      height: 700px;
+      background-color: transparent;
+    }
+  }
 }
 </style>
