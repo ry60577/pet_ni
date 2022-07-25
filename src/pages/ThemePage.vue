18944335428ddbe54b3a395d195538f32478318c
Astrid Chen <astrid.chen@flowring.com>
2022-07-25 08:57:54 +0800

    feat:新增主題館UI

--- src/pages/ThemePage.vue
+++ src/pages/ThemePage.vue
@@ -3,6 +3,18 @@
     class="q-pa-md theme_wrapper"
     :class="{ 'pw-sm': isMobile, 'pw-xl': !isMobile }"
   >
+    <div class="pet_type">
+      <span
+        :class="{ unselect: petType === 'dog' }"
+        @click="selectPetType('cat')"
+        >喵星人</span
+      >
+      <span
+        :class="{ unselect: petType === 'cat' }"
+        @click="selectPetType('dog')"
+        >汪星人</span
+      >
+    </div>
     <div class="theme_carousel theme_bg">
       <q-carousel
         swipeable
@@ -71,20 +83,88 @@ const themeData = themeList.map((item) => {
 });
 const slide = ref("orange_tabby_cat");
 const autoplay = ref(false);
+const petType = ref("cat");
+const selectPetType = (type) => {
+  petType.value = type;
+};
 </script>
 
 <style lang="scss" scoped>
 .theme_wrapper {
-  height: 100vh;
+  height: 100%;
   background-color: #e5e5e5;
+  .pet_type {
+    position: relative;
+    right: 0;
+    padding: 4px 0;
+    width: 150px;
+    text-align: center;
+    background-color: #fff;
+    box-shadow: 0px 2px 7px rgba(10, 10, 10, 0.07);
+    border-radius: 10px;
+    span {
+      padding: 2px 10px;
+      &.unselect {
+        width: 62px;
+        height: 26px;
+        color: #fff;
+        border-radius: 10px;
+        background-color: $dark;
+      }
+    }
+  }
 
   .carousel_card {
+    height: 100%;
     display: grid;
-    grid-template-columns: 50% 50%;
-    // grid-template-rows: 70% 30%;
-    // div:first-child {
-    //   order: 2;
-    // }
+    grid-template-columns: 55% 45%;
+    align-items: center;
+    h3 {
+      margin: 56px 0;
+      font-weight: 700;
+      font-size: 76px;
+      line-height: 27px;
+      letter-spacing: 0.374px;
+      color: $dark;
+    }
+    .desc {
+      margin: 48px 0;
+      font-weight: 400;
+      font-size: 18px;
+      line-height: 27px;
+      letter-spacing: 0.374px;
+      color: #878787;
+    }
+    :deep {
+      .q-btn {
+        width: 196px;
+        height: 46px;
+        background-color: #fff;
+        box-shadow: 0px 2px 7px rgba(10, 10, 10, 0.07);
+        border-radius: 16px;
+        .q-btn__content {
+          display: flex;
+          justify-content: space-around;
+          .block {
+            margin-right: 20px;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 24px;
+            color: $dark;
+          }
+        }
+      }
+      .q-btn .q-icon,
+      .q-btn .q-spinner {
+        position: absolute;
+        right: 10px;
+        width: 37px;
+        height: 37px;
+        color: white;
+        border-radius: 16px;
+        background-color: #262626;
+      }
+    }
   }
   .theme_bg {
     background-image: url("../assets/image/cat_chineses.svg");
@@ -95,6 +175,16 @@ const autoplay = ref(false);
     .q-carousel {
       height: 700px;
       background-color: transparent;
+      .q-carousel__control {
+        column-gap: 24px;
+        display: flex;
+        .q-btn {
+          width: 42px;
+          height: 42px;
+          box-shadow: 0px 2px 7px rgba(10, 10, 10, 0.07);
+          border-radius: 14px;
+        }
+      }
     }
   }
 }
